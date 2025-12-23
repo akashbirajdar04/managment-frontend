2533f27d46d0cf8e0bd4c49b3c6b067c10f5696a
Akash0437 <akashbirajdar814@gmail.com>
2025-12-23 18:20:06 +0530

    mvppp

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import App from './App.jsx'
 import "./App.css"
 import "./index.css"
 import './style.css';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Toaster } from 'sonner';
+
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
     <Toaster position="top-right" richColors />
+    <SpeedInsights />
   </BrowserRouter>
 )
