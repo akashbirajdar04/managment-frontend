bf09f397b70527fdaf6431c3637dd3ad6ff30dc8
Akash0437 <akashbirajdar814@gmail.com>
2026-01-17 17:20:15 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import "./App.css"
 import "./index.css"
-import './style.css';
+import './style.css'
 
 import { Toaster } from 'sonner';
 
